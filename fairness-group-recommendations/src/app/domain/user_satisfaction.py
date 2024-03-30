58b7893948eec03871867dda026f3d290ddea0ad
tontonialberto <tontonialberto@outlook.it>
2024-03-30 02:02:43 +0100

    Implement UserSatisfaction

---
+++ fairness-group-recommendations/src/app/domain/user_satisfaction.py
@@ -0,0 +1,30 @@
+from typing import List
+from app.domain.dataset import Dataset, ItemId, UserId
+from app.domain.prediction.prediction import Prediction
+from app.domain.recommender import Recommender
+
+
+class UserSatisfaction:
+    def __init__(self, recommender: Recommender, prediction: Prediction, dataset: Dataset) -> None:
+        self.__recommender = recommender
+        self.__prediction = prediction
+        self.__dataset = dataset
+    
+    def get_satisfaction(self, user: UserId, group_recommendation: List[ItemId]) -> float:
+        # For each item in the group recommendation, get the rating of the user.
+        # If the user has not rated the item, use the prediction to get the rating.
+        group_list_user_ratings = []
+        for item in group_recommendation:
+            rating = self.__dataset.get_rating(user, item)
+            if rating == 0:
+                rating = self.__prediction.get_prediction(user, item)
+            group_list_user_ratings.append(rating)
+        
+        group_list_satisfaction = sum(group_list_user_ratings)
+        
+        user_recommendations = self.__recommender.get_recommendations(user, len(group_recommendation))
+        user_list_satisfaction = sum([rating for _, rating in user_recommendations])
+        
+        print(f"User {user}. User list sat = {user_list_satisfaction}. Group list sat = {group_list_satisfaction}")
+        
+        return group_list_satisfaction / user_list_satisfaction
---
+++ fairness-group-recommendations/src/test/domain/TestUserSatisfaction.py
@@ -0,0 +1,73 @@
+from typing import List, Tuple
+from unittest import TestCase
+from unittest.mock import ANY, Mock
+
+from app.domain.dataset import Dataset, ItemId
+from app.domain.prediction.prediction import Prediction
+from app.domain.recommender import Recommender
+from app.domain.user_satisfaction import UserSatisfaction
+
+
+class TestUserSatisfaction(TestCase):
+    def test_get_satisfaction_maximum_if_grouprecs_equal_to_userrecs(self):
+        items_user_recommendation: List[ItemId] = [42, 1, 3, 100]
+        ratings_user_recommendation: List[float] = [5, 4.5, 4.3, 4.2] 
+        user_recommendation = list(
+            zip(items_user_recommendation, ratings_user_recommendation)
+        )
+        group_recommendation: List[ItemId] = items_user_recommendation
+        # Suppose the user has not rated any of the items in the group recommendation.
+        user_actual_ratings: List[float] = [0, 0, 0, 0]
+        
+        user_recommender = Mock(spec=Recommender)
+        user_recommender.get_recommendations.return_value = user_recommendation
+        user_predictor = Mock(spec=Prediction)
+        user_predictor.get_prediction.side_effect = ratings_user_recommendation
+        dataset = Mock(spec=Dataset)
+        dataset.get_rating.side_effect = user_actual_ratings
+        satisfaction = UserSatisfaction(user_recommender, user_predictor, dataset)
+        
+        sat = satisfaction.get_satisfaction(ANY, group_recommendation)
+        
+        self.assertEqual(1, sat)
+        
+    def test_get_satisfaction_uses_prediction_on_grouprecs_if_user_has_not_rated_items(self) -> None:
+        items_user_recommendation: List[ItemId] = [7, 81, 666, 12]
+        ratings_user_recommendation: List[float] = [5, 5, 5, 5]
+        user_recommendation = list(zip(items_user_recommendation, ratings_user_recommendation))
+        group_recommendation: List[ItemId] = [42, 1, 3, 100]
+        # Suppose the user has not rated any of the items in the group recommendation.
+        user_actual_ratings: List[float] = [0, 0, 0, 0]
+        # Also suppose that the user doesn't like the items in the group recommendation.
+        user_predicted_ratings: List[float] = [1, 1, 1, 1]
+        
+        user_recommender = Mock(spec=Recommender)
+        user_recommender.get_recommendations.return_value = user_recommendation
+        user_predictor = Mock(spec=Prediction)
+        user_predictor.get_prediction.side_effect = user_predicted_ratings
+        dataset = Mock(spec=Dataset)
+        dataset.get_rating.side_effect = user_actual_ratings
+        satisfaction = UserSatisfaction(user_recommender, user_predictor, dataset)
+        
+        sat = satisfaction.get_satisfaction(ANY, group_recommendation)
+        
+        self.assertEqual(4 / 20, sat)
+        
+    def test_get_satisfaction_uses_dataset_on_grouprecs_if_user_has_rated_items(self) -> None:
+        items_user_recommendation: List[ItemId] = [7, 81, 666, 12]
+        ratings_user_recommendation: List[float] = [5, 5, 5, 5]
+        user_recommendation = list(zip(items_user_recommendation, ratings_user_recommendation))
+        group_recommendation: List[ItemId] = [42, 1, 3, 100]
+        # Suppose that the user has rated every item in the group recommendation.
+        user_actual_ratings: List[float] = [1, 1, 1, 1]
+        
+        user_recommender = Mock(spec=Recommender)
+        user_recommender.get_recommendations.return_value = user_recommendation
+        user_predictor = Mock(spec=Prediction)
+        dataset = Mock(spec=Dataset)
+        dataset.get_rating.side_effect = user_actual_ratings
+        satisfaction = UserSatisfaction(user_recommender, user_predictor, dataset)
+        
+        sat = satisfaction.get_satisfaction(ANY, group_recommendation)
+        
+        self.assertEqual(4 / 20, sat)
